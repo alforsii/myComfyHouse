51e0877134b93d122ca0b4d6895801f0a7b4951e
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-01-25 10:14:53 -0500

    update

--- app.js
+++ app.js
@@ -11,26 +11,42 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 hbs.registerPartials(__dirname + '/views/partials');
 
-app.get('/', (req, res, next) => {
-  console.log(' req', req.url);
-  const items = products.items.map(item => {
-    const {
-      sys: { id },
-      fields: {
-        title,
-        price,
-        image: {
-          fields: {
-            file: { image },
-          },
+const items = products.items.map(item => {
+  const {
+    sys: { id },
+    fields: {
+      title,
+      price,
+      image: {
+        fields: {
+          file: { image },
         },
       },
-    } = item;
-    // const image = item.image.fields.file.image;
-    return { id, title, price, image };
-  });
+    },
+  } = item;
+  // const image = item.image.fields.file.image;
+  return { id, title, price, image };
+});
+const item1 = items[0];
+const item2 = items[1];
+const item3 = items[2];
+//Home
+app.get('/home', (req, res, next) => {
+  // console.log(' req', req.url);
+  console.log('items', item1);
+  res.render('home', { title: 'Home', item1, item2, item3 });
+});
+//Collections
+app.get('/collections', (req, res, next) => {
+  // console.log(' req', items);
   //   console.log('items', items);
-  res.render('index', { title: 'Home', items });
+  res.render('collection', { title: 'Collections', items });
+});
+//About
+app.get('/about', (req, res, next) => {
+  // console.log(' req', req.url);
+  //   console.log('items', items);
+  res.render('about', { title: 'About', items });
 });
 
 app.listen(PORT, () => console.log(`Running on ${PORT}`));
